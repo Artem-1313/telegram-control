515a866a7c0780d97701b4681ee42d139a9a6950
Artem Khmil <Artem@ss>
2021-10-05 23:31:44 +0300

    add models for db

--- telegram_project/panel/models.py
+++ telegram_project/panel/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Telegrams(models.Model):
+    date_create = models.DateTimeField(default=timezone.now)
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Executors(models.Model):
+    unit = models.CharField(max_length=100)
+    status = models.BooleanField(default=False)
+    telegrams = models.ForeignKey(Telegrams, on_delete=models.CASCADE)
--- telegram_project/panel/views.py
+++ telegram_project/panel/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Telegrams, Executors
 
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'panel/index.html')
+    return render(request, 'panel/index.html', { 'tlg':Telegrams.objects.all(),'exec':Executors.objects.all()})
 
 
